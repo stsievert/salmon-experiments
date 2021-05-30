3b904dc338f74cc0468f95371724c41945003107
Scott <stsievert@users.noreply.github.com>
2021-05-30 14:24:58 -0500

    hit enter

--- response-rate-next/generate-embeddings.py
+++ response-rate-next/generate-embeddings.py
@@ -1,61 +1,120 @@
-#!/usr/bin/env python
-# coding: utf-8
-import sys
-from typing import List, Any, Dict, Union, Optional
-from copy import deepcopy
-from ast import literal_eval
 from pathlib import Path
-from typing import Tuple
-import json
-import yaml
+from typing import Tuple, List, Any, Dict
+from distributed import Client, as_completed
 
-import pandas as pd
-from dask.distributed import Client
-from dask.distributed import as_completed
 import numpy as np
+import pandas as pd
+from sklearn.utils import check_random_state
 import msgpack
 
-for path in [
-    Path.home() / "Developer" / "stsievert" / "salmon",
-    Path.home() / "salmon",
-]:
-    sys.path.append(str(path / "examples"))
-
-import run
-import offline
-
-DIR = Path("io/2021-03-27")
-
-def _check_version():
-    import salmon
-
-    assert "v0.5.2+22" in salmon.__version__
-    return True
+import response_model
+import targets
+from salmon.triplets.offline import OfflineEmbedding
+from responses import _cook_next
 
 
-def _literal_eval(x):
-    try:
-        return literal_eval(x)
-    except ValueError:
-        return x
+def random_responses(T: List[int], num=20_000, random_state=42) -> np.ndarray:
+    X = []
+    rng = np.random.RandomState(42)
+    for _ in range(num):
+        i_h, i_l, i_r = _random_query(n, rng)
+        h, l, r = T[i_h], T[i_l], T[i_r]
+        winner = response_model.alien_egg(h, l, r, random_state=rng)
+        assert winner in [0, 1]
+        ans = [i_h, i_l, i_r] if winner == 0 else [i_h, i_r, i_l]
+        X.append(ans)
+    return np.array(X).astype(int)
 
 
-def _get_dict(s: str) -> dict:
-    k_v = [kv.split("=") for kv in s.split("-")]
-    k_v2 = {k: _literal_eval(v) for k, v in k_v}
-    return k_v2
+def _X_test(T: List[int]) -> np.ndarray:
+    return random_responses(T, num=60_000, random_state=42)
 
 
-def _ident(d: dict) -> str:
-    d2 = sorted(tuple(d.items()))
-    d3 = [f"{k}={v}" for k, v in d2]
-    return "-".join(d3)
+def _random_query(n: int, random_state) -> Tuple[int, int, int]:
+    rng = check_random_state(random_state)
+    h, a, b = rng.choice(n, size=3, replace=False).astype(int)
+    return h, a, b
+
+
+def fit_estimator(
+    *,
+    X_train: np.ndarray,
+    X_test: np.ndarray,
+    n: int,
+    num_ans: int,
+    sampling,
+    noise_model: str,
+    d: int = 2,
+    max_epochs: int = 1_000_000,
+    shuffle_seed=None,
+    meta=None,
+    **kwargs,
+) -> Tuple[OfflineEmbedding, Dict[str, Any]]:
+    if meta is None:
+        meta = {}
+    assert sampling is not None
+    if sampling == "random":
+        assert shuffle_seed is not None
+        rng = np.random.RandomState(shuffle_seed)
+        rng.shuffle(X_train)
+    X_train = X_train[:num_ans]
+    assert X_train.shape == (num_ans, 3)
+
+    est = OfflineEmbedding(n=n, d=d, random_state=400, max_epochs=max_epochs, **kwargs)
+    est.fit(X_train, X_test)
+
+    est_kwargs = {f"est__{k}": v for k, v in kwargs.items()}
+    meta = {f"meta__{k}": v for k, v in meta.items()}
+    ret_dict = {
+        "len_X_train": len(X_train),
+        "len_X_test": len(X_test),
+        "n_train": len(X_train),
+        "n_test": len(X_test),
+        "n": n,
+        "d": d,
+        "num_ans": num_ans,
+        "sampling": sampling,
+        "shuffle_seed": shuffle_seed,
+        "noise_model": noise_model,
+        "num_ans": num_ans,
+        "est__random_state": est.random_state,
+        "est__max_epochs": est.max_epochs,
+        "est__noise_model": est.noise_model,
+        **est_kwargs,
+        **meta,
+    }
+    return est, ret_dict
+
+
+def _get_kwargs(nm: str) -> Dict[str, float]:
+    # From Sec 3.2/bottom of page 6 of the NEXT paper
+    # https://papers.nips.cc/paper/2015/file/89ae0fe22c47d374bc9350ef99e01685-Paper.pdf
+    if nm == "CKL":
+        return {"module__mu": 0.05}
+    elif nm in ["TSTE", "SOE"]:
+        return {}
+    raise ValueError(f"nm={nm} not in ['CKL', 'TSTE', 'SOE']")
+
+
+def _get_num_ans(n_answers, n, lower=None):
+    base = 10 * n
+    #  num_ans = list(range(1 * n, 10 * n, n))
+    num_ans = list(range(6 * n, 30 * n, 3 * n))
+    num_ans += list(range(30 * n, 100 * n, 10 * n))
+    num_ans += list(range(100 * n, 300 * n, 30 * n))
+    num_ans += list(range(300 * n, 1_000 * n, 100 * n))
+    num_ans = [n for n in num_ans if n <= n_answers]
+    if lower:
+        num_ans = [n for n in num_ans if lower <= n]
+    if max(num_ans) < 0.95 * n_answers:
+        num_ans += [n_answers]
+    return num_ans
 
 
 def _serialize(d):
     if isinstance(d, np.integer):
         return int(d)
-    if isinstance(d, np.float):
+    if isinstance(d, (np.float64, float, np.floating)):
         return float(d)
     if isinstance(d, list):
         return [_serialize(_) for _ in d]
@@ -64,194 +123,148 @@ def _serialize(d):
     return d
 
 
-def _prep():
-    import os
-    import torch
+def _check_version():
+    import salmon
 
-    threads = int(os.environ.get("OMP_NUM_THREADS", 1))
-    torch.set_num_threads(max(threads, 1))
-    return threads
-
-
-def _flatten(d, prefix=""):
-    if isinstance(d, (int, str, bytes, float)):
-        return d
-    elif isinstance(d, dict):
-        return {k: _flatten(v, prefix=f"{k}__") for k, v in d.items()}
-    elif isinstance(d, (list, tuple)):
-        raise ValueError()
-    raise ValueError()
-
-
-def _get_kwargs(nm: str) -> Dict[str, Any]:
-    # From section 3.2 of NEXT paper (last full paragraph on page 6)
-    if nm == "CKL":
-        return {"module__mu": 0.05}
-
-    assert nm in ("SOE", "GNMDS", "TSTE")
-    return {}
-
-
-def _get_config(suffix: str, targets=False):
-    with open(DIR / f"config_{suffix}.yaml") as f:
-        raw = yaml.safe_load(f)
-    if targets:
-        return raw["targets"]
-    raw.pop("targets")
-    rare = _flatten(raw)
-    return rare
-
-
-def _get_futures(client, dfs, random_state: int, d=2, config=None):
-    for k, df in dfs.items():
-        if "RR" in k:
-            continue
-        elif "RandomSampling" in k:
-            df = df.sample(frac=1, random_state=random_state)
-        else:
-            raise ValueError(f"unrecognized algorithm in key {k}")
-
-    cols = ["head", "winner", "loser"]
-    datasets = {k: df[cols].to_numpy() for k, df in dfs.items()}
-    html_targets = _get_config("RandomSampling-5", targets=True)
-    targets = [t.split("/")[-2].strip("i.png '") for t in html_targets]
-    print([len(v) for v in datasets.values()])
-    X_test = run._X_test()
-
-    print([len(v) for v in datasets.values()])
-    print(f"Total of {len(datasets)} items")
-
-    NUM_ANS = [n * i for i in range(1, 10)]
-    NUM_ANS += [n * i for i in range(10, 20, 2)]
-    NUM_ANS += [n * i for i in range(20, 50, 5)]
-    NUM_ANS += [n * i for i in range(50, 100, 10)]
-    NUM_ANS += [n * i for i in range(100, 400, 20) if n * i <= 7000]
-    assert max(NUM_ANS) <= 6600
-    NUM_ANS += [7000]
-
-    static = dict(
-        X_test=X_test,
-        dwell=1000,
-        verbose=1000,
-        random_state=random_state,
-        max_epochs=400_000,
-        d=d,
-    )
-
-    if random_state == 1:
-        keys = [k for k in datasets.keys() if "RR" in k]
-        a_datasets = {k: client.scatter(datasets[k]) for k in keys}
-        active_futures = [
-            client.submit(
-                offline._get_trained_model,
-                a_datasets[k],
-                n_responses=n_ans,
-                meta=config or {},
-                noise_model=nm,
-                ident=f"active-{nm}",
-                alg="active",
-                fname=k,
-                priority=np.random.uniform(low=0.5, high=1),
-                **static,
-                **_get_kwargs(nm),
-            )
-            for n_ans in deepcopy(NUM_ANS)
-            for nm in ["TSTE", "SOE", "CKL", "GNMDS"]
-            for k in keys
-        ]
-        print("len(af)", len(active_futures))
-    else:
-        active_futures = []
-
-    assert all("Random" in k or "RR" in k for k in dfs.keys())
-    fname = "RandomSampling-5_responses"
-    r_dataset = client.scatter(datasets[fname])
-    random_futures = [
-        client.submit(
-            offline._get_trained_model,
-            r_dataset,
-            n_responses=n_ans,
-            meta=config or {},
-            noise_model=nm,
-            ident=f"random-{nm}",
-            alg="random",
-            fname=fname,
-            priority=np.random.uniform(low=0.5, high=0.6) if random_state == 1 else 0,
-            **static,
-            **_get_kwargs(nm),
-        )
-        for n_ans in deepcopy(NUM_ANS)
-        for nm in ["TSTE", "SOE", "CKL", "GNMDS"]
-    ]
-
-    futures = random_futures + active_futures
-    print(f"len(futures) = {len(futures)}")
-    return futures
+    assert "v0.6.1rc3+7" in salmon.__version__
+    return True
 
 
 if __name__ == "__main__":
+    n = 90
+    T = targets.get(n)
+    X_test = _X_test(T)
+    dl = np.abs(X_test[:, 0] - X_test[:, 1])
+    dr = np.abs(X_test[:, 0] - X_test[:, 2])
+    correct = (dl < dr).mean()
+    print(f"Correct {100 * correct}% of the time")
 
-    DIR = Path("io/2021-03-27/")
-    # From run.py
-    config = {
-        "n": 30,
-        "d": 2,
-        "R": 1,
-        "dataset": "alien_eggs",
-        "random_state": 42,
-        "reaction_time": 0.0,
-        "init": True,
-        "max_queries": 7000,
-        "fname": "{alg}-{responses_per_sec}",
-    }
-    noise = "human"
-    n = 30
-    d = 2
-    dfs = {
-        f.name.replace(".csv", ""): pd.read_csv(f) for f in DIR.glob("*_responses.csv")
-    }
-    print([len(df) for df in dfs.values()])
-    assert all("7" not in k for k in dfs.keys()), dfs.keys()
+    # Generate random responses
+    R = random_responses(T, num=30_000, random_state=2032 ** 2)
+    N_ANS = _get_num_ans(len(R), n)
+    N_ANS = N_ANS[-10:]
 
-    #  offline._get_trained_model(
-    #  datasets["responses_RandomSampling"],
-    #  n_responses=10_000,
-    #  meta=_get_config("RandomSampling"),
-    #  noise_model="CKL",
-    #  module__mu=0.05,
-    #  **static,
-    #  )
-    #  sys.exit(0)
-    #  breakpoint()
-    #  r_dataset = datasets["responses_RandomSampling"]
-
-    client = Client("localhost:8786")
-    client.upload_file("offline.py")
-    _d = client.run(_check_version)
-    _d = client.run(_prep)
-    _d = list(_d.values())
-    assert all(_d), _d
-    print(_d)
-
-    SEEDS = [s + 1 for s in range(10)]
-    assert 1 in SEEDS and SEEDS[0] == 1
-    _futures = [
-        _get_futures(client, dfs, random_state=rs, d=d, config=config) for rs in SEEDS
+    #  static = dict(X_test=X_test, n=n, d=2, max_epochs=2)  # TODO: change max_epochs
+    static = dict(X_test=X_test, n=n, d=2, max_epochs=4_000_000)
+    random_futures = [
+        dict(
+            X_train=R,
+            num_ans=num_ans,
+            shuffle_seed=seed,
+            sampling="random",
+            noise_model=nm,
+            meta={"sampling_seed": seed, "alg": "random"},
+            **_get_kwargs(nm),
+            **static,
+        )
+        for nm in ["CKL", "SOE", "TSTE"]
+        for num_ans in N_ANS
+        for seed in range(20)
     ]
-    futures = sum(_futures, [])
+
+    def _get_train_data(p: Path) -> np.ndarray:
+        raw = msgpack.loads(p.read_bytes(), raw=False)
+        df = _cook_next(raw)
+        return df[["head", "winner", "loser"]].to_numpy().astype(int)
+
+    X_trains = {
+        f: _get_train_data(f)
+        for f in Path("next/io/2021-05-24/").glob("rate=*_responses.msgpack")
+    }
+    next_futures = [
+        dict(
+            X_train=X_train,
+            num_ans=num_ans,
+            sampling="next",
+            noise_model=nm,
+            meta={"alg": "TSTE", "vary": "rate", "fname": str(f)},
+            **_get_kwargs(nm),
+            **static,
+        )
+        for nm in ["CKL", "SOE", "TSTE"]
+        for f, X_train in X_trains.items()
+        for num_ans in _get_num_ans(len(X_train), n)
+    ]
+
+    X_trains = {
+        f: pd.read_csv(f)[["head", "winner", "loser"]].to_numpy().astype(int)
+        for f in Path("salmon/io/2021-05-25/").glob("*responses.csv.zip")
+    }
+    salmon_rates = [
+        dict(
+            X_train=X_train,
+            num_ans=num_ans,
+            sampling="salmon",
+            noise_model=nm,
+            meta={"alg": "ARR", "vary": "rate", "fname": str(f)},
+            **_get_kwargs(nm),
+            **static,
+        )
+        for nm in ["CKL", "SOE", "TSTE"]
+        for f, X_train in X_trains.items()
+        for num_ans in _get_num_ans(len(X_train), n)
+    ]
+    X_trains = {
+        f: pd.read_csv(f)[["head", "winner", "loser"]].to_numpy().astype(int)
+        for f in Path("salmon/io/2021-05-26-search/").glob("*responses.csv.zip")
+    }
+    salmon_searches = [
+        dict(
+            X_train=X_train,
+            num_ans=num_ans,
+            sampling="salmon",
+            noise_model=nm,
+            meta={"alg": "TSTE", "vary": "search", "fname": str(f)},
+            **_get_kwargs(nm),
+            **static,
+        )
+        for nm in ["CKL", "SOE", "TSTE"]
+        for f, X_train in X_trains.items()
+        for num_ans in _get_num_ans(len(X_train), n)
+    ]
+
+    for job in [salmon_searches, salmon_rates, next_futures, random_futures]:
+        print("len(job) =", len(job))
+    job_kwargs = salmon_searches + salmon_rates + next_futures + random_futures
+    print("len(job_kwargs) =", len(job_kwargs))
+
+    def _get_priority(d: Dict[str, Any]) -> float:
+        p = 20_000 / d["num_ans"]
+        if d["noise_model"] == "CKL":
+            p *= 10
+        if "n_search=1" in d["meta"].get("fname", ""):
+            p /= 100
+        return p
+
+    job_kwargs = list(sorted(job_kwargs, key=lambda d: -1 * _get_priority(d)))
+    keys = ["noise_model", "sampling", "num_ans"]  # , "meta"]
+    show = [{k: j[k] for k in keys} for j in job_kwargs]
+    for s, j in zip(show, job_kwargs):
+        s.update(j["meta"])
+    client = Client("localhost:8786")
+    d = client.run(_check_version)
+    assert all(list(d.values()))
+
+    futures = []
+    for k, kwargs in enumerate(job_kwargs):
+        kwargs["X_train"] = client.scatter(kwargs["X_train"])
+        kwargs["X_test"] = client.scatter(kwargs["X_test"])
+        future = client.submit(fit_estimator, **kwargs, priority=len(job_kwargs) - k)
+        futures.append(future)
+    print("len(futures) =", len(futures))
 
     for i, future in enumerate(as_completed(futures)):
         est, meta = future.result()
         meta = _serialize(meta)
-        fname = _ident(meta) + ".csv"
         save = {
             "embedding": est.embedding_.tolist(),
             "meta": meta,
             "performance": _serialize(est.history_[-1]),
             "history": _serialize(est.history_),
         }
-        _show = {k: est.history_[-1][k] for k in ["score_test", "loss_test"]}
-        show = {k: f"{v:0.3f}" for k, v in _show.items()}
-        print(i, meta["alg"], meta["ident"], meta["n_train"], show)
-        with open(f"/scratch/ssievert/io/alien-eggs/{i}.msgpack", "wb") as f2:
+        _keys = ["noise_model", "sampling", "num_ans", "meta__alg"]
+        print(f"{i} / {len(job_kwargs)}", {k: meta.get(k, "") for k in _keys})
+        with open(
+            f"/scratch/ssievert/next-comparison/io-cluster/{i}.msgpack", "wb"
+        ) as f2:
             msgpack.dump(save, f2)
--- response-rate-next/responses.py
+++ response-rate-next/responses.py
@@ -1,14 +1,27 @@
 import pandas as pd
 from typing import List
+import numpy as np
 
-def _cook_next(raw: List[dict]) -> pd.DataFrame:
-    rare = list(raw["participant_responses"].values())
+
+def _cook_next(raw, byte_keys=False) -> pd.DataFrame:
+    try:
+        rare = list(raw["participant_responses"].values())
+    except:
+        rare = list(raw[b"participant_responses"].values())
     mrare = sum(rare, [])
     mwell = []
     for response in mrare:
         if "target_winner" not in response:
             continue
-        query = {k: response.get(k, -999) for k in ["alg_id", "network_delay", "response_time", "timestamp_query_generated"]}
+        query = {
+            k: response.get(k, -999)
+            for k in [
+                "alg_id",
+                "network_delay",
+                "response_time",
+                "timestamp_query_generated",
+            ]
+        }
         query["puid"] = response["participant_uid"]
         objs = response["target_indices"]
         l = [o for o in objs if o["label"] == "left"][0]
@@ -18,5 +31,10 @@ def _cook_next(raw: List[dict]) -> pd.DataFrame:
         for k, v in [("head", h), ("left", l), ("right", r), ("winner", winner)]:
             query[f"{k}"] = v["target_id"]
             query[f"{k}_obj"] = v["primary_description"]
+            query[f"{k}_smooth"] = int(v["primary_description"].strip("i.png"))
         mwell.append(query)
-    return pd.DataFrame(mwell)
+    df = pd.DataFrame(mwell)
+    df["loser"] = df["right"].copy()
+    right_wins = df["winner"] == df["right"]
+    df.loc[right_wins, "loser"] = df.loc[right_wins, "left"]
+    return df
